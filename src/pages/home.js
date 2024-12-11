61cf36c4f7c0ae19a5d72717b87e968bc0633dba
Nguyễn Thị Hiền Hạnh <nthhanh0504@gmail.com>
2024-12-11 18:31:54 +0700

    commit on master branch

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,3 +2,4 @@ console.log("home.js");
 console.log("conflict user");
 console.log("make commit with error message");
 console.log("make commit file with correct message");
+console.log("make commit on master branch");
